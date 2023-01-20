a144f3aa3fefc4621de81457f0d215309eeefce5
Rx JoseM <51371604+JOS3GAMER1@users.noreply.github.com>
2023-01-19 21:33:10 -0500

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,5 @@
+const tl1 = gsap.timeline({defaults:{ease:"steps.out"}})
+tl1.to('.text',{y:"0%",duration:3, stagger:0.50})
+tl1.to('.slider',{y:"-100%", duration:1.5, delay:0.30})
+tl1.to('.intro',{y:"-100%",duration:1,},"-=1")
+tl1.fromTo('nav',{opacity:0},{opacity:1, duration:0.5})
